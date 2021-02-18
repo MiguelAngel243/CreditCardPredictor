7b0819bff9840f9051997a931f8611c008c091a8
Miguel <maggen420@gmail.com>
2021-02-17 20:50:30 -0600

    Para Rev. 3 Shiny

--- Predictor/app.R
+++ Predictor/app.R
@@ -13,40 +13,49 @@ ui <- dashboardPage(
         )
     ),
     dashboardBody(
-        tabItems(
-            tabItem(tabName = "Predecir",
-                    fluidRow(
+            tabItems(
+                tabItem(tabName = "Predecir",
+                        fluidRow(
+                        box(id="d", title="Ingreso de Datos",collapsible = TRUE, width = 19,
                         tabBox(
-                            title="Ingreso de Datos",
                             id="tabset1",
                             tabPanel("Por Cliente",
                                      box(title="Datos Demográficos",
                                          selectInput("gensel","Género",c("Hombre","Mujer")),
                                          selectInput("educsel","Grado Educativo",c("Licenciatura","Maestría/Doctorado","Bachillerato","Otro")),
                                          selectInput("marsel","Estado Marital",c("Soltero","Casado","Otro"))
+                                     ),box(title="Datos Financieros",
+                                           numericInput("limitbal","Límite de Crédito",0),
+                                           "Ingrese los siguientes datos mensuales, comenzando por el mes pasado (1):",
+                                           matrixInput("financ",value = m,cols = list(
+                                               names = TRUE
+                                           ),
+                                           rows = list(
+                                               names = TRUE
+                                           ))
+                                           
                                      ),
-                                     box(title="Datos Financieros",
-                                         numericInput("limitbal","Límite de Crédito",0),
-                                         matrixInput("financ",value = m,cols = list(
-                                             names = TRUE
-                                         ),
-                                         rows = list(
-                                             names = TRUE
-                                         ))
-                                         
-                                     )
+                                     submitButton("Calcular")
                             ),
                             tabPanel("Por Lote",
                                      fileInput("filesel","Archivo de Datos", buttonLabel = "Seleccionar")),
-                            height=250
+                            height=650,
+                            width=19
                         )
-                    )
-            ),
-            tabItem(tabName = "Configurar")
-        )
+                        ),
+                        box(title="Resultados", width=19)
+                        )
+                ),
+                tabItem(tabName = "Configurar",
+                        numericInput("rel","Relación de Costo FP/FN:",3))
+            )
+        
+        
     )
 )
 
-server <- function(input, output) { }
+server <- function(input, output) { 
+    
+    }
 
 shinyApp(ui, server)
